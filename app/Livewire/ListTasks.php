a93d1e39bd78236992e998d7b236dbace91a78e4
Ben Everly <ben.everly@oberd.com>
2023-08-13 22:28:11 -0500

    task list

--- app/Livewire/ListTasks.php
+++ app/Livewire/ListTasks.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire;
 
+use App\Models\Task;
 use Livewire\Component;
 
 class ListTasks extends Component
 {
     public function render()
     {
-        return view('livewire.list-tasks');
+        return view('livewire.list-tasks', [
+            'tasks' => Task::all(),
+        ]);
     }
 }
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <title>Livewire</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="https://cdn.tailwindcss.com"></script>
-    </head>
-    <body class="antialiased">
-        {{ $slot }}
-    </body>
+
+<head>
+    <title>Livewire</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="antialiased">
+    {{ $slot }}
+</body>
+
 </html>
