2dba1e0ded5b2c0aaafaa7532d6d386cafc4edd5
Ben Everly <ben.everly@oberd.com>
2023-08-13 21:08:50 -0500

    initialize livewire

---
+++ app/Livewire/ListTasks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ListTasks extends Component
+{
+    public function render()
+    {
+        return view('livewire.list-tasks');
+    }
+}
