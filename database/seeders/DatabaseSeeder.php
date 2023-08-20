e8836c5425db6a92e2d8a5d9b4a839e351b46cda
Ben Everly <ben.everly@oberd.com>
2023-08-19 22:32:10 -0500

    update db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Status;
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Status::factory()
+            ->count(4)
+            ->create()
+            ->each(function (Status $status) {
+                Task::factory()->count(3)->for($status)->create();
+            });
     }
 }
