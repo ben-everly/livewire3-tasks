623d74f8353cda760b4bedcaa408814549131a3a
Ben Everly <ben.everly@oberd.com>
2023-08-23 23:10:42 -0500

    install tailwind

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <title>Livewire</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
 </head>
 
 <body class="antialiased">
