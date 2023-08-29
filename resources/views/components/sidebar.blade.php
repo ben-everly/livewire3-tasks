900425008c85834fa21578966debc8b9e09efdfe
Ben Everly <ben.everly@oberd.com>
2023-08-28 20:31:16 -0500

    replace user name placeholder

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -36,7 +36,7 @@
                             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                             alt="">
                         <span class="sr-only">Your profile</span>
-                        <span aria-hidden="true">Tom Cook</span>
+                        <span aria-hidden="true">{{ auth()->user()->name }}</span>
                     </a>
                 </li>
             @endif
