9e62e7e5c16d11e66a84cc6ee8d52a001f57b8cf
Ben Everly <ben.everly@oberd.com>
2023-09-09 15:54:52 -0500

    more styling

--- resources/views/livewire/list-tasks.blade.php
+++ resources/views/livewire/list-tasks.blade.php
@@ -2,21 +2,22 @@
     <table class="min-w-full">
         <thead>
             <tr>
-                <th class="p-2"
+                <th class="p-1"
                     scope="col">
                     Id
                     </td>
-                <th class="p-2"
+                <th class="p-1"
                     scope="col">
                     Description
                 </th>
-                <th class="p-2"
+                <th class="p-1"
                     scope="col">
                     Status
                 </th>
-                <th class="flex flex-row-reverse p-2"
+                <th class="flex flex-row-reverse p-1"
                     scope="col">
-                    <button class="rounded-full bg-indigo-600 p-1 text-slate-50 hover:bg-indigo-700"
+                    <button
+                        class="bg-slate-100 p-1 text-slate-600 hover:bg-slate-200 dark:bg-slate-800 dark:text-slate-300 dark:hover:bg-slate-700"
                         type="button"
                         wire:click="createTask">
                         <x-icons.plus-circle />
@@ -27,18 +28,19 @@
         <tbody>
             @foreach ($tasks as $index => $task)
                 <tr wire:key="task-row-{{ $task->id }}">
-                    <td class="whitespace-nowrap p-2 font-medium">
+                    <td class="whitespace-nowrap p-1 font-medium">
                         {{ $task->team->abbreviation }}-{{ $task->number }}
                     </td>
-                    <td class="p-2 font-medium">
+                    <td class="p-1 font-medium">
                         {{ $task->description }}
                     </td>
-                    <td class="whitespace-nowrap p-2 font-medium">
+                    <td class="whitespace-nowrap p-1 font-medium">
                         <livewire:task-status-select wire:key="task-status-{{ $task->id }}"
                             :task="$task" />
                     </td>
-                    <td class="relative whitespace-nowrap p-2 text-right font-medium">
-                        <button class="text-slate-300 bg-slate-800 hover:bg-slate-700 p-1"
+                    <td class="relative whitespace-nowrap p-1 text-right font-medium">
+                        <button
+                            class="bg-slate-100 p-1 text-slate-700 hover:bg-slate-200 dark:bg-slate-800 dark:text-slate-300 dark:hover:bg-slate-700"
                             wire:click="editTask({{ $task }})">
                             <x-icons.pencil class="h-4 w-4" />
                             <span class="sr-only">{{ $task->description }}</span>
